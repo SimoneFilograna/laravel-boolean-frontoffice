dd98534ae91afd0a5792542afa7a20cfc9548b94
PasqualeIsabelli <pasquale0395@gmail.com>
2023-10-20 15:25:20 +0200

    add header and footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,32 @@
 <script>
-    export default{
+export default {
 
-    }
+}
 </script>
 
 <template>
-    <h1>FOOTER</h1>
+  <div class="container">
+    <div class="d-flex justify-content-between nav-box py-3">
+      <div >
+        <img style="height: 80px;" src="../assets/images/nav-logo.png">
+      </div>
+      <div class="pt-5">
+        <p class="fs-6">Via Brombeis, 23, Napoli</p>
+      </div>
+      <div class="pt-5">
+        <ul class="d-flex list-unstyled gap-3">
+          <li class="text-decoration-none fs-6">Privacy Policy</li>
+          <li class="text-decoration-none fs-6">|</li>
+          <li class="text-decoration-none fs-6">Site Map</li>
+          <li class="text-decoration-none fs-6">|</li>
+          <li class="text-decoration-none fs-6">Website Accesibility</li>
+          <li class="text-decoration-none fs-6">|</li>
+          <li class="text-decoration-none fs-6">Tracking preferences</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,63 @@
 <script>
-    export default{
-
+export default {
+  data() {
+    return {
+      images: ['../assets/images/slider1.jpeg', '../assets/images/slider2.jpeg']
     }
+  },
+  methods: {
+    getImg(img) {
+      return new URL(img, import.meta.url).href
+    }
+  }
+}
 </script>
 
 <template>
-    <h1>HEADER</h1>
+  <nav>
+    <div class="container">
+      <div class="d-flex justify-content-between nav-box py-3">
+        <img src="../assets/images/nav-logo.png">
+        <ul class="d-flex list-unstyled gap-3 pt-3">
+          <li><router-link class="color text-decoration-none fs-5" :to="{ name: 'home' }">Home</router-link></li>
+          <!-- <li><router-link class="text-decoration-none fs-5" :to="{ name: 'cocktails'}">Cocktail</router-link></li> -->
+          <!-- <li><router-link class="text-decoration-none fs-5 :to="{ name: 'cocktail.show' }">Cocktail</router-link></li> -->
+          <li><router-link class="color text-decoration-none fs-5" :to="{ name: 'about_us' }">Chi Siamo</router-link></li>
+          <li><router-link class="color text-decoration-none fs-5" :to="{ name: 'contacts' }">Contatti</router-link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid p-0">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="5000" v-for="image in images">
+          <img style="height: 650px;" class="d-block w-100" :src="getImg(image)">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+.nav-box {
+  height: 100px;
+  
+  .color {
+    color: black;
+    &:hover {
+      color: rgb(109, 96, 21);
+    }
+  }
+}
 
 </style>
