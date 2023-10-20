f0c70b2430100c360971083ac3b9ee352ce697bf
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-20 15:14:41 +0200

    FIX-COMP-APP

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script>
-  export default{
+  import TheHeader from './components/TheHeader.vue';
+  import TheFooter from './components/TheFooter.vue';
 
+  export default{
+    components: {
+            TheHeader,
+            TheFooter,
+        }
   }
 </script>
 
 <template>
+  <TheHeader></TheHeader>
   <router-view></router-view>
+  <TheFooter></TheFooter>
 </template>
 
 <style lang="scss">
+
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,13 @@
 <script>
-    import TheHeader from '../components/TheHeader.vue';
-    import TheFooter from '../components/TheFooter.vue';
     export default{
-        components: {
-            TheHeader,
-            TheFooter,
-        }
+        
     }
 </script>
 
 <template>
-    <TheHeader></TheHeader>
+    
     <h1>HELLO FRONtoffice</h1>
-    <TheFooter></TheFooter>
+    
 </template>
 
 <style lang="scss" scoped>
