b7941b5ac0c4360731c08e962474f6cb098813b2
MatteoArt <mattrim8@gmail.com>
2023-10-20 15:58:29 +0200

    recupero dati axios

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,33 @@
 <script>
-    export default{
-        
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            cocktailsList: []
+        }
+    },
+    methods: {
+        fetchData() {
+            axios.get('http://127.0.0.1:8000/api/cocktails')
+                .then((response) => {
+                    //ottengo i dati relativi ai cocktails
+                    this.cocktailsList = response.data;
+
+                    console.log(this.cocktailsList);
+                })
+        }
+    },
+    mounted() {
+        this.fetchData();
     }
+}
 </script>
 
 <template>
-    
     <h1>HELLO FRONtoffice</h1>
+
     
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
