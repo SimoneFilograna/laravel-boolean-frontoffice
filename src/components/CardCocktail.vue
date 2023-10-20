eb7aceffece25691f0234710254325fbbb03b8a1
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-20 17:58:34 +0200

    card-component-and basic style-card

--- src/components/CardCocktail.vue
+++ src/components/CardCocktail.vue
@@ -1,12 +1,47 @@
 <script>
     export default{
-
+        props:{
+            singleDrink : {
+                type: Object,
+                require: true,
+            }
+        },
     }
 </script>
 
 <template>
-
+        <router-link :to='{ name:"cocktail.show", params: {id: singleDrink.id}}'>
+        <div class="card text-white border-0">
+            <img :src="singleDrink.thumb" class="card-img" :alt="singleDrink.name">
+            <div class="card-img-overlay text-center">    
+                <div class="center-container">
+                    <h2 class="card-title">{{singleDrink.name}}</h2>
+                    <p class="card-text"><small>{{ singleDrink.category }}</small></p>      
+                </div>         
+            </div>
+        </div>
+        </router-link>
 </template>
 
 <style lang="scss" scoped>
+        .card-img-overlay{
+            display: flex;
+            flex-direction: column;
+            opacity: 0;
+            transition: opacity .6s;
+            backdrop-filter: brightness(50%);
+        &:hover {
+            opacity: 1;
+        }    
+            .center-container{
+                margin-top: 30%;
+            }
+            .text-card{
+                width: 33%;
+                .img-back{
+                    object-fit: contain;
+                }
+            }
+        }
+    
 </style>
--- src/pages/Cocktails.vue
+++ src/pages/Cocktails.vue
@@ -1,33 +1,43 @@
 <script>
-import axios from 'axios'
+    import axios from 'axios'
+    import CardCocktail from '../components/CardCocktail.vue'
 
-    export default{
+    export default {
+        components:{
+            CardCocktail,
+        },           
         data() {
-        return {
-            cocktailsList: [],
-        }
-    },
-    methods: {
-        fetchData() {
-            axios.get('http://127.0.0.1:8000/api/cocktails')
-                .then((response) => {
-                    //ottengo i dati relativi ai cocktails
-                    this.cocktailsList = response.data;
+            return {
+                cocktailsList: [],
+            }
+        },
+        methods: {
+            fetchData() {
+                axios.get('http://127.0.0.1:8000/api/cocktails')
+                    .then((response) => {
+                        //ottengo i dati relativi ai cocktails
+                        this.cocktailsList = response.data;
 
-                    console.log(this.cocktailsList);
-                })
+                        console.log(this.cocktailsList);
+                    })
+            }
+        },
+        mounted() {
+            this.fetchData();
         }
-    },
-    mounted() {
-        this.fetchData();
-    }
     }
 </script>
 
 <template>
     
     <h1 class="text-center">Cocktails</h1>
-    
+    <div class="container">
+        <div class="row row-cols-3 g-5 justify-content-center">
+            <div class="col" v-for="cocktail in cocktailsList" :key="cocktail.id">
+                <CardCocktail :singleDrink="cocktail"></CardCocktail>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
