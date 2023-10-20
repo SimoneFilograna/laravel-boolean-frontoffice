cdbd187754d90ca21ce27764e3c58c0a64a753bd
PasqualeIsabelli <pasquale0395@gmail.com>
2023-10-20 17:06:23 +0200

    add aboutus page

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,13 +1,25 @@
 <script>
-    export default{
+export default {
 
-    }
+}
 </script>
 
 <template>
-    <h1>CHI SIAMO</h1>
+    <div class="container">
+        <h1 class="color mt-4 text-center fw-bold">Cocktail Lounge</h1>
+        <div class="col-8 m-auto">
+            <div class="d-flex my-5 gap-5">
+                <div class="image-box">
+                    <img style="height: 400px;" src="../assets/images/main.jpeg">
+                </div>
+                <div class="desc-box">
+                    <p>Attraversando il famoso varco di Via Brombeis di Napoli, si raggiunge il Cocktail Lounge, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi rerum porro laudantium dolores officia repellendus placeat voluptate dignissimos unde laboriosam quod dolor nobis, voluptas optio, eius provident quas adipisci! Sequi? At velit distinctio, delectus sapiente odio corrupti? In libero nisi similique voluptates quis. Laudantium voluptatum ipsa, at perferendis ratione modi numquam, saepe cupiditate facere suscipit inventore nobis ut aperiam. Quas. Reprehenderit error sequi obcaecati? Quisquam, vitae quod. Illum at tempore officiis dolor. Minus consectetur perferendis ut tenetur asperiores velit cum laudantium perspiciatis, voluptas dolor porro omnis autem obcaecati tempora consequatur!</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped>.color {
+    color: #80725f;
+}</style>
