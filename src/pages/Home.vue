439b9380a98c47457c0db9f1b3df88b902fa0a4e
ElisaIanda <elisaianda01@gmail.com>
2023-10-21 16:02:14 +0200

    Add Home Cocktail

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,34 @@
 <script>
 import Slider from '../components/Slider.vue';
+import axios from 'axios'
+import CardCocktail from '../components/CardCocktail.vue'
+
 
 export default {
     components: {
         Slider,
+        CardCocktail,
     },
     data() {
         return {
+            cocktailsList:{},
         }
+    },
+    methods: {
+        fetchData() {
+            axios.get('http://127.0.0.1:8000/api/cocktails')
+                .then((response) => {
+                    //ottengo i dati relativi ai cocktails
+                    this.cocktailsList = response.data.slice(0 , 2);
+
+                    console.log(this.cocktailsList);
+                })
+        }
+
+    },
+    
+    mounted() {
+        this.fetchData();
     }
 }
 </script>
@@ -16,6 +37,11 @@ export default {
     <Slider></Slider>
     <div class="container mt-4">
         <h1 class="text-center">HOME</h1>
+        <div class="row row-cols-3 g-5 justify-content-center">
+            <div class="col" v-for="cocktail in cocktailsList" :key="cocktail.id">
+                <CardCocktail :singleDrink="cocktail"></CardCocktail>
+            </div>
+        </div>
     </div>
 </template>
 
